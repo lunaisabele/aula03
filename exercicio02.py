8b9e99fe031ba99d0ea7e6d3b1c19a39c0565c34
luna <isabeleluna16@gmail.com>
2025-03-31 15:12:39 -0300

    operaçoes matematicas com dois numeros

---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+# codigo para ler dois numeros efetuar 4 operaçoes matematicas e mostrar os resultados
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+
+soma = num1 + num2
+subt = num1 - num2
+mult = num1 * num2
+divi = num1 / num2
+
+print(f"soma: {num1} + {num2} = {soma} \nsubtraçao: {num1} - {num2} = {subt}\n"
+      f"multiplicaçao: {num1} * {num2} = {mult}\ndivisao: {num1} / {num2} = {divi}")
