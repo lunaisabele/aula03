f1cd070e5adc83dca2224812e50ebbffe9c58ed9
luna <isabeleluna16@gmail.com>
2025-03-31 16:16:04 -0300

    receber dois numeros e mostrar eles em ordem crescente exercicio04

---
+++ exercicio4.py
@@ -0,0 +1,7 @@
+# codigo para receber dois numeros e mostrar eles em ordem crecente
+num1 = int(input(f"Digite um numero: "))
+num2 = int(input(f"Digite um numero: "))
+if num1 > num2:
+    print(num2,num1)
+else:
+ print(num1, num2)
