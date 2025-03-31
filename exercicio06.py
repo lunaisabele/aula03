5901fe77a254830e88cde576adac99d90ffad20d
luna <isabeleluna16@gmail.com>
2025-03-31 17:24:23 -0300

    atualizao do exercicio06

--- exercicio06.py
+++ exercicio06.py
@@ -1,13 +1,14 @@
 # codigo para receber o nome de dois times e dizer qual é o vencedor
 time1 = input("Nome do primeiro time: ")
-time2 = input("Nome do segundo time: ")
-#numero de gols de cada time
 goldotime1 = int(input( "digite o numero de gols do time 1: "))
+time2 = input("Nome do segundo time: ")
 goldotime2 = int(input( "digite o numero de gols do time 2: "))
 
-if goldotime1>goldotime2:
-    print(f"vencedor: {time1}")
-elif goldotime2 < goldotime1:
-    print(f"vencedor:{time2}")
-else:
+
+if goldotime1==goldotime2:
     print("empate")
+elif goldotime2 > goldotime1:
+    print(f"vencedor:{time1}")
+else:
+    print(f"vencedor: {time2}")
+
