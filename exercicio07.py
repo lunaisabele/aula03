c2430ca8779a07437c5be51b767c8a6b17c402d9
luna <isabeleluna16@gmail.com>
2025-04-02 14:43:52 -0300

    atualizaçao para aceitar as letras menusculas exercicio07

--- exercicio07.py
+++ exercicio07.py
@@ -4,14 +4,14 @@ litros = float(input("digete a quantidade de litros: "))
 vgas = 5.8
 veta = 4.9
 
-if tipodecombustivel == "G":
+if tipodecombustivel == "G" or tipodecombustivel =="g":
     calculodagasolina = litros * vgas
-    print(f"voce abasteceu {litros} "
-          f"e gastou {calculodagasolina}" )
-elif tipodecombustivel == "E" :
+    print(f"voce abasteceu {litros}"
+           f" e gastou R${calculodagasolina}" )
+elif tipodecombustivel == "E" or tipodecombustivel =="e":
     calculodoetanol = litros * veta
     print(f"voce abasteceu {litros}" 
-      f"e gastou {calculodoetanol}" )
+          f"e gastou R${calculodoetanol}" )
 else:
     print("solicitacao invalida" )
 
